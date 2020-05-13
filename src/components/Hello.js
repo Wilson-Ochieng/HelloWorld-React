5bf65440b6af28330e61eba17f5984850e82ad66
wuodmwalimu <wilsonochieng718@gmail.com>
2020-05-13 15:56:29 +0300

    commit changes giving same output without JSX   components

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,10 +1,14 @@
 import React from 'react'
- const Hello = () =>
-{
-    return(
-        <div>
-            <h1>Hello Willybiz</h1>
-        </div>
+ const Hello = () =>{
+    // return(
+    //     <div>
+    //         <h1>Hello Willybiz</h1>
+    //     </div>
+    // )
+    return React.createElement(
+        'div',
+    {id:'HelloObject',className:'Dummyclass'},
+    React.createElement('h1',null,'Hello Willybiz')
     )
 }
 export default Hello
